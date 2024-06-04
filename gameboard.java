44a9e4a290849f5c1baf9ab55bf7c7447ed92981
midK-night <130721380+midK-night@users.noreply.github.com>
2024-06-04 10:39:03 -0500

    start coding gameboard

--- gameboard.java
+++ gameboard.java
@@ -1,7 +1,41 @@
 import pieces;
-import 
+import java.util.ArrayList;
 
 
 public class gameboard {
-    private static ArrayList<
+    private static ArrayList<base> whitePieces;
+    private static ArrayList<base> blackPieces;
+    private static boolean isPVP;
+    public static String[][] chessboard;
+
+    public gameboard(boolean isPVP) {
+        this.isPVP = isPVP;
+        chessboard = new String[8][8];
+        for (int i = 0; i < 8; i++) {
+            whitePieces.add(new Pawn(i, 1));
+            blackPieces.add(new Pawn(i, 6));
+        }
+    }
+
+    public static void chessboardUpdate() {
+        String[][] tempChessboard = new String[8][8];
+        for (String[] row : tempChessboard) {
+            for (String box : row) {
+                box = "#";
+            }
+        } // reset to #
+        for (base i : whitePieces) {
+            int[] pos = i.returnPos();
+            if (!i.getCaptured()) {
+                tempChessboard[pos[0]][pos[1]] = i.getInit();
+            }
+        } // set white pieces
+        for (base i : blackPieces) {
+            int[] pos = i.returnPos();
+            if (!i.getCaptured()) {
+                tempChessboard[pos[0]][pos[1]] = i.getInit();
+            }
+        } // set black pieces
+        chessboard = tempChessboard;
+    }
 }
